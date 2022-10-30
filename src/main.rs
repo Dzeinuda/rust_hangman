c20f9fde122de34166692b1499a532edabe8be86
Nicolas Konrad Voigt Grunwald <nicolasgrunwald@yahoo.dk>
2022-10-30 11:46:51 +0100

    More visuals

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,21 @@ use std::io;
 extern crate reqwest;
 
 fn main() {
-    welcome_message();  
+    welcome_message();
     let target_word: String = strip_first_and_last(get_word());
     let mut lives: u8 = 8;
-    
+
     let mut hidden_word: Vec<char> = Vec::new();
     let mut guess: Vec<char> = Vec::new();
     let mut wrong_guess: Vec<char> = Vec::new();
-  
+
     for l in target_word.chars() {
         hidden_word.push(l);
         match l {
             ' ' => guess.push(' '),
             _ => guess.push('_')
-        }  
-    }  
-    
+        }
+    }
     loop {
         if lives == 0 {
             clear_term();
@@ -27,18 +26,18 @@ fn main() {
         } else if &hidden_word == &guess {
             clear_term();
             println!("The word was '{}', you win!\n", target_word);
+            escape();
             break
         }
-         
+
         clear_term();
         gallows(lives);
-            
         println!("Guess a letter! (lives left: {})", lives);
         println!("{:?}", guess);
-        
+
         let mut player_guess: String = String::new();
         io::stdin().read_line(&mut player_guess).expect("Failed to read line");
-        
+
         let player_guess: char = match player_guess.trim().parse() {
             Ok(letter) => letter,
             Err(_) => {
@@ -46,7 +45,7 @@ fn main() {
                 continue
             },
         };
-        
+
         if hidden_word.contains(&player_guess) {
             println!("{} is in the word!\n", player_guess);
             for i in 0..hidden_word.len() {
@@ -59,7 +58,7 @@ fn main() {
             println!("You already tried that, no lives deducted!\n");
             continue
         }
-        
+
         wrong_guess.push(player_guess);
         println!("{} is not in the word!\n", player_guess);
         lives = lives - 1;
@@ -68,26 +67,26 @@ fn main() {
 
 fn welcome_message() -> () {
     clear_term();
-    println!("Welcome to Hangman, fetching a word...");
+    println!("Welcome to Hangman, fetching a word... ");
 }
 
 fn strip_first_and_last(input: String) -> String {
-    
-    /* This function is silly and should not be 
-    needed, todo: find a better way to remove 
+
+    /* This function is silly and should not be
+    needed, todo: find a better way to remove
     quotation marks around JSON return word */
-    
+
     let first_last_off: &str = &input[1..input.len() - 1];
     String::from(first_last_off)
 }
 
 fn get_word() -> String {
-    
+
     /* Retrieve a random word from online API.
     This should be made, such that the code does
-    not panick on errors. Match the Result enum 
+    not panick on errors. Match the Result enum
     variants instead*/
-    
+
     let data = reqwest::blocking::get("https://randomword-gen.herokuapp.com/api/six")
         .expect("Could not execute request.")
         .text()
@@ -96,101 +95,116 @@ fn get_word() -> String {
 }
 
 fn clear_term() -> () {
-    print!("{}[2J", 27 as char);   
+    print!("{}[2J", 27 as char);
 }
 
-fn gallows(life: u8) -> () { 
+fn gallows(life: u8) -> () {
     match life {
         8 => {
-            println!("░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░");
-            println!("░░░░░░░███████░░░░░░░░░░░░░░░░░░░░░░");
-            println!("░░░░░░░█░░░░░░░░░░░░░░░░░░░░░░░░░░░░");
-            println!("░░░░░░░█░░░░░░░░░░░░░░░░░░░░░░░░░░░░");
-            println!("░░░░░░░█░░░░░░░░░░░░░░░░░░░░░░░░░░░░");
-            println!("░░░░░░░█░░░░░░░░░░░░░░░░░░░░░░░░░░░░");
-            println!("░░░░░░░█░░░░░░░░░░░░░░░░░░░░░░░░░░░░");
-            println!("░░░░░░░█░░░░░░░░░░░░░░░░░░░░░░░░░░░░");
+            println!("                                    ");
+            println!("       ███████                      ");
+            println!("       █                            ");
+            println!("       █                            ");
+            println!("       █                            ");
+            println!("       █                            ");
+            println!("       █                            ");
+            println!("_______█____________                ");
         }
         7 => {
-            println!("░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░");
-            println!("░░░░░░░███████░░░░░░░░░░░░░░░░░░░░░░");
-            println!("░░░░░░░█░░░░│░░░░░░░░░░░░░░░░░░░░░░░");
-            println!("░░░░░░░█░░░░░░░░░░░░░░░░░░░░░░░░░░░░");
-            println!("░░░░░░░█░░░░░░░░░░░░░░░░░░░░░░░░░░░░");
-            println!("░░░░░░░█░░░░░░░░░░░░░░░░░░░░░░░░░░░░");
-            println!("░░░░░░░█░░░░░░░░░░░░░░░░░░░░░░░░░░░░");
-            println!("░░░░░░░█░░░░░░░░░░░░░░░░░░░░░░░░░░░░");
+            println!("                                    ");
+            println!("       ███████                      ");
+            println!("       █    │                       ");
+            println!("       █                            ");
+            println!("       █                            ");
+            println!("       █                            ");
+            println!("       █                            ");
+            println!("_______█____________                ");
         }
         6 => {
-            println!("░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░");
-            println!("░░░░░░░███████░░░░░░░░░░░░░░░░░░░░░░");
-            println!("░░░░░░░█░░░░│░░░░░░░░░░░░░░░░░░░░░░░");
-            println!("░░░░░░░█░░░░█░░░░░░░░░░░░░░░░░░░░░░░");
-            println!("░░░░░░░█░░░░░░░░░░░░░░░░░░░░░░░░░░░░");
-            println!("░░░░░░░█░░░░░░░░░░░░░░░░░░░░░░░░░░░░");
-            println!("░░░░░░░█░░░░░░░░░░░░░░░░░░░░░░░░░░░░");
-            println!("░░░░░░░█░░░░░░░░░░░░░░░░░░░░░░░░░░░░");
+            println!("                                    ");
+            println!("       ███████                      ");
+            println!("       █    │                       ");
+            println!("       █    █                       ");
+            println!("       █                            ");
+            println!("       █                            ");
+            println!("       █                            ");
+            println!("_______█____________                ");
         }
         5 => {
-            println!("░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░");
-            println!("░░░░░░░███████░░░░░░░░░░░░░░░░░░░░░░");
-            println!("░░░░░░░█░░░░│░░░░░░░░░░░░░░░░░░░░░░░");
-            println!("░░░░░░░█░░░░█░░░░░░░░░░░░░░░░░░░░░░░");
-            println!("░░░░░░░█░░░░┼░░░░░░░░░░░░░░░░░░░░░░░");
-            println!("░░░░░░░█░░░░░░░░░░░░░░░░░░░░░░░░░░░░");
-            println!("░░░░░░░█░░░░░░░░░░░░░░░░░░░░░░░░░░░░");
-            println!("░░░░░░░█░░░░░░░░░░░░░░░░░░░░░░░░░░░░");
+            println!("                                    ");
+            println!("       ███████                      ");
+            println!("       █    │                       ");
+            println!("       █    █                       ");
+            println!("       █    ┼                       ");
+            println!("       █                            ");
+            println!("       █                            ");
+            println!("_______█____________                ");
         }
         4 => {
-            println!("░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░");
-            println!("░░░░░░░███████░░░░░░░░░░░░░░░░░░░░░░");
-            println!("░░░░░░░█░░░░│░░░░░░░░░░░░░░░░░░░░░░░");
-            println!("░░░░░░░█░░░░█░░░░░░░░░░░░░░░░░░░░░░░");
-            println!("░░░░░░░█░░░─┼░░░░░░░░░░░░░░░░░░░░░░░");
-            println!("░░░░░░░█░░░░░░░░░░░░░░░░░░░░░░░░░░░░");
-            println!("░░░░░░░█░░░░░░░░░░░░░░░░░░░░░░░░░░░░");
-            println!("░░░░░░░█░░░░░░░░░░░░░░░░░░░░░░░░░░░░");
+            println!("                                    ");
+            println!("       ███████                      ");
+            println!("       █    │                       ");
+            println!("       █    █                       ");
+            println!("       █   ─┼                       ");
+            println!("       █                            ");
+            println!("       █                            ");
+            println!("_______█____________                ");
         }
         3 => {
-            println!("░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░");
-            println!("░░░░░░░███████░░░░░░░░░░░░░░░░░░░░░░");
-            println!("░░░░░░░█░░░░│░░░░░░░░░░░░░░░░░░░░░░░");
-            println!("░░░░░░░█░░░░█░░░░░░░░░░░░░░░░░░░░░░░");
-            println!("░░░░░░░█░░░─┼─░░░░░░░░░░░░░░░░░░░░░░");
-            println!("░░░░░░░█░░░░░░░░░░░░░░░░░░░░░░░░░░░░");
-            println!("░░░░░░░█░░░░░░░░░░░░░░░░░░░░░░░░░░░░");
-            println!("░░░░░░░█░░░░░░░░░░░░░░░░░░░░░░░░░░░░");
+            println!("                                    ");
+            println!("       ███████                      ");
+            println!("       █    │                       ");
+            println!("       █    █                       ");
+            println!("       █   ─┼─                      ");
+            println!("       █                            ");
+            println!("       █                            ");
+            println!("_______█____________                ");
         }
         2 => {
-            println!("░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░");
-            println!("░░░░░░░███████░░░░░░░░░░░░░░░░░░░░░░");
-            println!("░░░░░░░█░░░░│░░░░░░░░░░░░░░░░░░░░░░░");
-            println!("░░░░░░░█░░░░█░░░░░░░░░░░░░░░░░░░░░░░");
-            println!("░░░░░░░█░░░─┼─░░░░░░░░░░░░░░░░░░░░░░");
-            println!("░░░░░░░█░░░░│░░░░░░░░░░░░░░░░░░░░░░░");
-            println!("░░░░░░░█░░░░░░░░░░░░░░░░░░░░░░░░░░░░");
-            println!("░░░░░░░█░░░░░░░░░░░░░░░░░░░░░░░░░░░░");
+            println!("                                    ");
+            println!("       ███████                      ");
+            println!("       █    │                       ");
+            println!("       █    █                       ");
+            println!("       █   ─┼─                      ");
+            println!("       █    │                       ");
+            println!("       █                            ");
+            println!("_______█____________                ");
         }
         1 => {
-            println!("░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░");
-            println!("░░░░░░░███████░░░░░░░░░░░░░░░░░░░░░░");
-            println!("░░░░░░░█░░░░│░░░░░░░░░░░░░░░░░░░░░░░");
-            println!("░░░░░░░█░░░░█░░░░░░░░░░░░░░░░░░░░░░░");
-            println!("░░░░░░░█░░░─┼─░░░░░░░░░░░░░░░░░░░░░░");
-            println!("░░░░░░░█░░░░│░░░░░░░░░░░░░░░░░░░░░░░");
-            println!("░░░░░░░█░░░│░░░░░░░░░░░░░░░░░░░░░░░░");
-            println!("░░░░░░░█░░░░░░░░░░░░░░░░░░░░░░░░░░░░");
+            println!("                                    ");
+            println!("       ███████                      ");
+            println!("       █    │                       ");
+            println!("       █    █                       ");
+            println!("       █   ─┼─                      ");
+            println!("       █    │                       ");
+            println!("       █   │                        ");
+            println!("_______█____________                ");
         }
         0 => {
-            println!("░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░");
-            println!("░░░░░░░███████░░░░░░░░░░░░░░░░░░░░░░");
-            println!("░░░░░░░█░░░░│░░░░░░░░░░░░░░░░░░░░░░░");
-            println!("░░░░░░░█░░░░█░░░░░░░░░░░░░░░░░░░░░░░");
-            println!("░░░░░░░█░░░─┼─░░░░░░░░░░░░░░░░░░░░░░");
-            println!("░░░░░░░█░░░░│░░░░░░░░░░░░░░░░░░░░░░░");
-            println!("░░░░░░░█░░░│░│░░░░░░░░░░░░░░░░░░░░░░");
-            println!("░░░░░░░█░░░░░░░░░░░░░░░░░░░░░░░░░░░░");
+            println!("                                    ");
+            println!("       ███████                      ");
+            println!("       █    │                       ");
+            println!("       █    █                       ");
+            println!("       █   ─┼─                      ");
+            println!("       █    │                       ");
+            println!("       █   │ │                      ");
+            println!("_______█____________                ");
         }
-        _ => println!("What?")
+        _ => {
+            println!("Seems to be an issue...");
+        }
     }
+    println!("");
+}
+
+fn escape() -> () {
+    println!("                                    ");
+    println!("       ███████                      ");
+    println!("       █    │     I'm free!         ");
+    println!("       █          /                 ");
+    println!("       █        █                   ");
+    println!("       █       ─┼─                  ");
+    println!("       █        │                   ");
+    println!("_______█_______│ │____              ");
+    println!("");
 }
